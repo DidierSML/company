2763b38a5cd9293e033f210f68e544ab6897b6db
DidierSML <didiersmaldonad@gmail.com>
2023-11-13 10:48:27 -0500

    first commit

---
+++ src/main/java/com/example/company/model/Empleados.java
@@ -0,0 +1,22 @@
+package com.example.company.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empleados {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private double sueldo;
+    private int experiencia;
+    private int telefono;
+    private String email;
+
+
+}
